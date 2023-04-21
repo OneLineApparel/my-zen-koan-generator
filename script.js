a22a37a9eb07c10f63b8d507759d75508a7ac8c2
Matan <admin@Matans-MBP.fios-router.home>
2023-04-21 12:46:45 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.getElementById("koan-form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const theme = document.getElementById("theme").value;
+  generateZenKoan(theme);
+});
+
+async function generateZenKoan(theme) {
+  const message = {
+    "role": "system",
+    "content": `write me a 10-word zen koan about "${theme}":`
+  };
+
+  const response = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer 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" // Include the API key in the Authorization header (use your own key)
+    },
+    body: JSON.stringify({
+      messages: [message],
+      max_tokens: 50,
+      n: 1,
+      stop: null,
+      temperature: 0.8,
+      model: "gpt-3.5-turbo"
+    }),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    const message = data.choices[0].message;
+    if (message && message.content) {
+      const koan = message.content.trim().split('\n')[0];
+      result.textContent = `Generated Zen Koan: "${koan}"`;
+    } else {
+      result.textContent = "An error occurred. Please try again.";
+    }
+  } else {
+    result.textContent = "An error occurred. Please try again.";
+  }
+}
